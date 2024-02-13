71273ae33a9309336936809e15d5ed15670a6fc1
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-02-13 19:00:59 +0200

    Create Book Model

---
+++ app/Models/Book.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Book extends Model
+{
+    use HasFactory;
+    protected $table = "books";
+    protected $fillable = ['title', 'lang', 'size', 'rate' ,  'category_id', 'user_id', 'page', 'image', 'created_at'];
+    protected $hidden = ['created_at'];
+
+    public $timestamps = true;
+}
