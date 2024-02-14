ce0e7d010bf4ddf5f6a96397bfa4d32d8829f153
Mohamed abdelhaleam <mohamedabdelhaleam13@gmail.com>
2024-02-14 20:48:02 +0200

    Add Public Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Auth\AuthController;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -26,4 +29,25 @@ Route::group([
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::post('/refresh', [AuthController::class, 'refresh']);
     Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+Route::group([
+    'prefix' => 'categories'
+], function () {
+    Route::get('', [CategoryController::class, 'index']);
+    Route::get('category-books/{categoryId}', [BookController::class, 'getBooksInCategory']);
+});
+Route::group([
+    'prefix' => 'books'
+], function () {
+    Route::get('', [BookController::class, 'index']);
+    Route::get('{bookId}', [BookController::class, 'getBook']);
+    Route::get('books-in-category/{categoryId}', [BookController::class, 'getBook']);
+    Route::get('books-by-author/{authorId}', [BookController::class, 'getBooksByAuthor']);
+});
+Route::group([
+    'prefix' => 'author'
+], function () {
+    Route::get('', [AuthorController::class, 'index']);
+    Route::get('{authorId}', [AuthorController::class, 'getAuthor']);
+    Route::get('books-by-author/{authorId}', [BookController::class, 'getBooksByAuthor']);
 });
